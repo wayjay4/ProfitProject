9f5dff6f07c8871e67e2758bcaf81b4034661bcd
Waylon Dixon <wdixon@waylondixon.com>
2017-09-15 21:49:21 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="PHP Exercise - File Processor">
+    <meta name="author" content="Waylon Dixon">
+
+    <title>My Profit Project</title>
+
+    <!-- Custom styles for this template -->
+    <link href="/css/app.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="container">
+
+      <?php
+
+      echo("Hello World!");
+
+      ?>
+
+    </div><!-- /.container -->
+
+    <script src="/js/app.js"></script>
+
+  </body>
+</html>
